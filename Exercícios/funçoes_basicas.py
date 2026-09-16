990e82cfc2a02eee65f9e3bf639c6350a13e2c39
JESSICA FABRES LINDNER DA SILVA <84614455034@senacrs.edu.br>
2026-09-15 21:58:06 -0300

    Implementaçao exercícios Python

--- Exercícios/funçoes_basicas.py
+++ Exercícios/funçoes_basicas.py
@@ -1,8 +1,30 @@
-def formatar_saudacao(nome:str, cidade:srt):
+#Exercício 1
+
+def formatar_saudacao(nome:str, cidade:str):
     return f"Olá {nome},seja bem-vindo(a) a {cidade}!"
 
+#Exercício 2
+def calcular_perimetro(largura:float,altura:float):
+    perimetro = 2*(largura+altura)
+    return perimetro
+
+#Exercicio 3
+def fahrenheit_para_celcius(temp_f: float):
+    temp_celcius = (temp_f -32)
+    return gorjeta
+
+#Exercicio 4
+def alcular_gorjeta_por_pessoa(conta:float, porcentagem_gorjeta:float, pessoas:int): 
+    gorjeta = conta* (porcentagem_gorjeta/100) / pessoas
+    return gorjeta
+
+
 if __name__ =='__main__': 
-    saudacao = formatar_saudacao('Alice, Porto Alegre')
-    
-    print(saudacao)
-    
+
+    print("EXERCICIOS=============================== \n\n")
+    saudacao = formatar_saudacao('Alice', 'Porto Alegre')
+    print(f"1 - {saudacao}")
+    perimetro = calcular_perimetro(altura=10, largura=5)
+    print(f"2 - {perimetro}")
+    print("==================================================================")
+
