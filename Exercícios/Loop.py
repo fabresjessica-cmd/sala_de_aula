3e684ee55e7438f4f4d9b127a49c9c9a8464ef23
JESSICA FABRES LINDNER DA SILVA <84614455034@senacrs.edu.br>
2026-09-21 21:52:17 -0300

    exercicios em andamento

--- Exercícios/Loop.py
+++ Exercícios/Loop.py
@@ -3,6 +3,30 @@ def dobrar(numeros:[]):
         numero = numero*2
         print (numero)
 
+#exercício 1
+
+def filtrar_pares(numeros):
+    lista_pares = []
+    for num in numeros:
+    if num % 2 == 0:
+        lista_pares.append(num)
+    return lista_pares
+
+#Exercicio 2
+
+def contar_negativos(numeros):
+    contador = 0
+    for num in numeros:
+    if num < 0:
+       contador += 1
+       return contador
+    
+#exercicio 3
+
+def zerar_negativos(numeros):
+    
+
+
 if __name__=='__main__':
     dobrar ([1,2,3,4,5])
     
--- Exercícios/condicionais.py
+++ Exercícios/condicionais.py
@@ -15,11 +15,51 @@ def verificar_maioridade(idade:int):
         return "menor_de_idade"
     
 def verificar_paridade(numero:int):
+    if numero > 0: 
+        return "positivo"
+    elif numero < 0:
+        return "negativo"
+    else:
+        return "zero"
+    
+
+def classificar_numero(numero):
+    if numero > 0:
+        return "Positivo"
+    elif numero < 0:
+        return "Negativo"
+    else:
+        return "Zero"
+ 
+def calcular_resultado(nota_1:float,nota_2:float ):
+    if (nota_1 + nota_2)/ 2>7:
+        return "aprovado" 
+    else:
+        return "reprovado"
+    
+     
+def maior_de_dois(a, b):
+    if a > b:
+        return "O primeiro é maior"
+    elif b > a:
+        return "O segundo é maior"
+    else:
+        return "São iguais"
+
+def calcular_desconto(valor_compra:float, e_cliente_vip):
+    if e_cliente_vip or valor_compra > 200:
+        desconto = 0.85 # 15%
+        return f"Valor final: R$ {valor_compra*desconto}"
+    else:
+        desconto = 0.05 # 5%
+        valor_final  
+
+    
 
 
-    if __name__ =="__main__":
-     teste = fizz_buzz(15)
-     print (teste)
+if __name__ =="__main__":
+ teste = fizz_buzz(15)
+print (teste)
 
 
     
